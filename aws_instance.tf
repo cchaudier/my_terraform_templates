a4b83daf72d59ddcb6b7f5e1540c72e2e2943de8
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-10-03 22:27:50 +0200

    refactoring by ressources

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,26 +1,3 @@
-# Our default security group to access
-# the instances over SSH and HTTP
-resource "aws_security_group" "default" {
-    name = "terraform_example"
-    description = "Used in the terraform"
-
-    # SSH access from anywhere
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    # HTTP access from anywhere
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
 resource "aws_instance" "web" {
 
   instance_type = "${var.aws_instance_type}"
