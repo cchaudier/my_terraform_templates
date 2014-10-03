89579530b64d1ec4e1169f35acf964cb1daad724
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-10-03 21:54:37 +0200

    create provider file

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-	access_key = "${var.aws_access_key}"
-	secret_key = "${var.aws_secret_key}"
-	region = "us-east-1"
-}
-
 resource "aws_vpc" "default" {
 	cidr_block = "10.0.0.0/16"
 }
