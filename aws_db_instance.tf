1d353e24e54a807d2be66254a0045466bf339e67
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-10-05 18:21:52 +0200

    add posgres RDS

---
+++ aws_db_instance.tf
@@ -0,0 +1,23 @@
+resource "aws_db_instance" "db" {
+  identifier = "mydb-rds"
+  
+  #Type de la base
+  engine = "postgres"
+  engine_version = "9.3.3"
+  
+  #Taille
+  instance_class = "db.t1.micro"
+  #instance_class = "db.t2.micro"
+  allocated_storage = 5
+
+  #Base de donnée à créer
+  name = "mydb"
+  username = "admin_ccr"
+  password = "admin_ccr"
+
+  #Backup
+
+  #Securité
+  #publicly_accessible = "true"
+  #security_group_names = ["${aws_db_security_group.bar.name}"]
+}
