d5ed3239ec9a52d2b2c8f1e0683973cbc1a5b8eb
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-10-05 19:58:39 +0200

    add db info

--- output.tf
+++ output.tf
@@ -1,7 +1,19 @@
+output "db_address" {
+  value = "${aws_db_instance.db.adresse}"
+}
+
+output "db_endpoint" {
+  value = "${aws_db_instance.db.endpoint}"
+}
+
 output "web_public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
 
 output "web_public_dns_name" {
   value = "${aws_instance.web.public_dns}"
+}
+
+output "elastic_public_ip" {
+  value = "${aws_eip.web_ip.public_ip}"
 }
