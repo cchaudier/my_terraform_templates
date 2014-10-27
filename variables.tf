8bf2d1fb8279fa4f78a1d490b768285b56a49689
Christophe Chaudier <chaudier.christophe@gmail.com>
2014-10-27 10:58:14 +0100

    available zones

--- variables.tf
+++ variables.tf
@@ -5,6 +5,15 @@ variable "aws_key_name" {}
 variable "aws_region" {
   default = "eu-west-1" 
 }
+
+variable "aws_zone" {
+  default = {
+    a = "eu-west-1a"
+    b = "eu-west-1b"
+    c = "eu-west-1c"
+  }
+}
+
 variable "aws_instance_type" {
   default = "t2.micro" 
 }
@@ -18,3 +27,8 @@ variable "aws_ami" {
 # Ubuntu Precise 14.04 LTS (x64) EBS only for t2.micro
   default = "ami-20903157"
 }
+
+variable "aws_db_name" { }
+variable "aws_db_username" { }
+variable "aws_db_password" { }
+
